200c048ead6a9ad1ce452e5a5c92f43b3e50a678
Alfredoeb9 <alfredoeb96@gmail.com>
2024-02-05 10:25:16 -0800

    [initial]

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,25 +4,25 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { env } from "@/env";
 import * as schema from "./schema";
 
-// export const db = drizzle(
-//   new Client({
-//     url: env.DATABASE_URL,
-//   }).connection(),
-//   { schema }
-// );
+export const db = drizzle(
+  new Client({
+    url: env.DATABASE_URL,
+  }).connection(),
+  { schema }
+);
 
 
 
 
-import 'dotenv/config'
-// import { drizzle } from 'drizzle-orm/planetscale-serverless'
-import { connect } from '@planetscale/database'
+// import 'dotenv/config'
+// // import { drizzle } from 'drizzle-orm/planetscale-serverless'
+// import { connect } from '@planetscale/database'
 
-// create the connection
-const connection = connect({
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD
-})
+// // create the connection
+// const connection = connect({
+//   host: process.env.DATABASE_HOST,
+//   username: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD
+// })
 
-const db = drizzle(connection, { schema })
+// const db = drizzle(connection, { schema })
